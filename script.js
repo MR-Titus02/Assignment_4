df5d3935f721ab30f8425a3dea5ebd7e8c0db16b
MR-Titus02 <titusroxsan@gmail.com>
2025-03-02 15:00:33 +0530

    added javascript btns & filter

---
+++ script.js
@@ -0,0 +1,97 @@
+function showall() {
+    //for show all
+    var back5 = document.getElementById("back1")
+    back5.classList.remove("hide")
+    var back6 = document.getElementById("back2")
+    back6.classList.remove("hide")
+    var back7 = document.getElementById("back3")
+    back7.classList.remove("hide")
+    var back8 = document.getElementById("back4")
+    back8.classList.remove("hide")
+    var front5 = document.getElementById("front1")
+    front5.classList.remove("hide")
+    var front6 = document.getElementById("front2")
+    front6.classList.remove("hide")
+    var front7 = document.getElementById("front3")
+    front7.classList.remove("hide")
+    var front8 = document.getElementById("front4")
+    front8.classList.remove("hide")
+    //coloring for buttons
+    document.getElementById("btn1").style.backgroundColor = "#F76B0E";
+    document.getElementById("btn2").style.backgroundColor = "white";
+    document.getElementById("btn3").style.backgroundColor = "white";
+    document.getElementById("btn2").style.color = "black";
+    document.getElementById("btn3").style.color = "black";
+
+
+}
+function showfront() {
+    var back5 = document.getElementById("back1")
+    back5.classList.remove("hide")
+    var back6 = document.getElementById("back2")
+    back6.classList.remove("hide")
+    var back7 = document.getElementById("back3")
+    back7.classList.remove("hide")
+    var back8 = document.getElementById("back4")
+    back8.classList.remove("hide")
+    var front5 = document.getElementById("front1")
+    front5.classList.remove("hide")
+    var front6 = document.getElementById("front2")
+    front6.classList.remove("hide")
+    var front7 = document.getElementById("front3")
+    front7.classList.remove("hide")
+    var front8 = document.getElementById("front4")
+    front8.classList.remove("hide")
+    //for resetting above lines
+    var back1 = document.getElementById("back1")
+    back1.classList.add("hide")
+    var back2 = document.getElementById("back2")
+    back2.classList.add("hide")
+    var back3 = document.getElementById("back3")
+    back3.classList.add("hide")
+    var back4 = document.getElementById("back4")
+    back4.classList.add("hide")
+    //coloring for buttons
+    document.getElementById("btn1").style.backgroundColor = "white";
+    document.getElementById("btn2").style.backgroundColor = "#F76B0E";
+    document.getElementById("btn3").style.backgroundColor = "white";
+    document.getElementById("btn1").style.color = "black";
+    document.getElementById("btn2").style.color = "white";
+    document.getElementById("btn3").style.color = "black";
+
+
+}
+function showback() {
+    var back5 = document.getElementById("back1")
+    back5.classList.remove("hide")
+    var back6 = document.getElementById("back2")
+    back6.classList.remove("hide")
+    var back7 = document.getElementById("back3")
+    back7.classList.remove("hide")
+    var back8 = document.getElementById("back4")
+    back8.classList.remove("hide")
+    var front5 = document.getElementById("front1")
+    front5.classList.remove("hide")
+    var front6 = document.getElementById("front2")
+    front6.classList.remove("hide")
+    var front7 = document.getElementById("front3")
+    front7.classList.remove("hide")
+    var front8 = document.getElementById("front4")
+    front8.classList.remove("hide")
+    //for resetting above lines
+    var front1 = document.getElementById("front1")
+    front1.classList.add("hide")
+    var front2 = document.getElementById("front2")
+    front2.classList.add("hide")
+    var front3 = document.getElementById("front3")
+    front3.classList.add("hide")
+    var front4 = document.getElementById("front4")
+    front4.classList.add("hide")
+    //coloring for buttons
+    document.getElementById("btn1").style.backgroundColor = "white";
+    document.getElementById("btn2").style.backgroundColor = "white";
+    document.getElementById("btn3").style.backgroundColor = "#F76B0E";
+    document.getElementById("btn1").style.color = "black";
+    document.getElementById("btn3").style.color = "white";
+    document.getElementById("btn2").style.color = "black";
+}
